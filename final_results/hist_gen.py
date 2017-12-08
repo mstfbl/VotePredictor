7e363b1d53ca6951e98b08a0a074cc6e3a28806c
tchang <chang.thomas1@gmail.com>
2017-12-08 08:09:28 -0500

    file cleanup

--- final_results/hist_gen.py
+++ final_results/hist_gen.py
@@ -4,6 +4,10 @@ import sys
 with open(sys.argv[1], 'r') as f:
   lines = f.readlines()
   legislators = [float(line) for line in lines if ":" not in line]
-      
-matplotlib.pyplot.hist(legislators, bins = 10)
+
+
+matplotlib.pyplot.hist(legislators, bins = 20)
+axes = matplotlib.pyplot.gca()
+axes.set_xlim([0.0,1.0])
+
 matplotlib.pyplot.savefig(sys.argv[1].split('.')[0] + '_hist.png')
